c6f73f263e76da7a3b8a93766c0be692963e9302
Chen Wang <chenw@cmu.edu>
2017-04-02 19:08:52 -0400

    Create monitorData folder in logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,10 +1,16 @@
 import json
 import time
+import os
 import client_config
 from utils.client_utils import *
 
 def logJson(fileNamePrefix, data):
     dataFolder = client_config.monitor_trace_folder
+    try:
+        os.stat(dataFolder)
+    except:
+        os.mkdir(dataFolder)
+
     client_name = getMyName()
     cur_ts = time.strftime("%m%d%H%M%S")
     fileName = dataFolder + fileNamePrefix + "_" + client_name + "_" + cur_ts + ".json"
